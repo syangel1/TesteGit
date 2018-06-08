0075192eab1cec36a58f0440cbf6f10550117ad3
laboratorio <laboratorio@LaboratorioA16.labifms.edu.br>
2018-06-07 21:20:44 -0400

    primeiro commit

---
+++ src/testegit/TesteGit.java
@@ -0,0 +1,19 @@
+
+package testegit;
+
+/**
+ *
+ * @author laboratorio
+ */
+public class TesteGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Minicurso GIT!");
+        System.out.println("Semana do Meio Ambiente 2018!!!");
+        
+    }
+    
+}
